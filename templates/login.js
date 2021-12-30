40993e6ee08dd2e88d084b2055a9bc435de5cb3e
JerryKim <ecec1023@gmail.com>
2021-12-30 09:47:19 +0900

    login.js에 주석 달고, login.html이랑 sign_up.html에 input에 있는 id 중복값이 있어서 수정#6

--- templates/login.js
+++ templates/login.js
@@ -6,7 +6,8 @@ const linkToMain = document.getElementsByTagName('a')[0];
 idInput.addEventListener('keyup', function(event) {
     if (idInput.value && pwInput.value) {
         loginBtn.disabled = false;
-        linkToMain.href = "피드 화면 링크";
+        // 로그인 버튼 활성화 시 클릭할 때 깃헙에 저장된 index.html로 링크 걸어놓음
+        linkToMain.href = "https://github.com/JerryKim1023/16team_instagram_flask/blob/366991713314adec6b20102c2b3f3c08bb61bb82/templates/index.html";
     }
     else {
         loginBtn.disabled = true;
@@ -17,7 +18,8 @@ idInput.addEventListener('keyup', function(event) {
 pwInput.addEventListener('keyup', function(event) {
     if (idInput.value && pwInput.value) {
         loginBtn.disabled = false;
-        linkToMain.href = "피드 화면 링크";
+        // 로그인 버튼 활성화 시 클릭할 때 깃헙에 저장된 index.html로 링크 걸어놓음
+        linkToMain.href = "https://github.com/JerryKim1023/16team_instagram_flask/blob/366991713314adec6b20102c2b3f3c08bb61bb82/templates/index.html";
     }
     else {
         loginBtn.disabled = true;
@@ -25,6 +27,7 @@ pwInput.addEventListener('keyup', function(event) {
     }
 })
 
+// 엔터키 사용을 위한 설정 / 엔터의 keyCode가 13
 document.addEventListener('keyup', function(event) {
     if (event.keyCode === 13) {
         document.getElementById("btn_login").click();
@@ -33,6 +36,6 @@ document.addEventListener('keyup', function(event) {
 
 // 인스타 로고 클릭시 홈메뉴로 복귀
 function to_main() {
-    window.location.href = "/"
+    window.location.href = "https://pienergy.shop"
 }
 
