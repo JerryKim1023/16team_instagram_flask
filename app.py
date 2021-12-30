5e16ca522dbd8e23d3548bb2544e8de5f8b17afa
KIMHOONHEE <nmdkims@gmail.com>
2021-12-31 00:59:32 +0900

    수정 및 추가 - app.py ajax이용 클라이언트와 연결, feedindex창에서 random_show.js 실행, random_show.js 추가 : 피드인덱스 화면에서 스토리, 팔로우 목록 랜덤 출력하는 기능 #12

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
+from pymongo import MongoClient
 
 app = Flask(__name__)
+
+client = MongoClient('mongodb+srv://AKBARI:sparta@cluster0.jujbu.mongodb.net/cluster0?retryWrites=true&w=majority')
+db = client.dbakbari
+
+import hashlib
 
 
 @app.route('/')
@@ -8,14 +14,46 @@ def main():
     return render_template("login.html")  # 처음 페이지
 
 
+@app.route("/api/random_show", methods=['POST'])
+def show_random():
+    rest_list = []
+    count = int(request.form['count_give'])
+    rest = list(db.rest.find({}, {'_id': False}).sort('like', -1))
+    for count_ in rest[count:count + 8]:
+        rest_list.append(count_)
+    return jsonify({'rest': rest_list})
+
+
 @app.route('/feedindex')
 def detail():
     return render_template("feedindex.html")  # 상세페이지로 이동
 
 
-@app.route('/sign_up')
+# 회원가입 부분 구현
+@app.route('/sign_up', methods=['POST'])
 def sign_up():
-    return render_template('sign_up.html')  # 로그인 등록 작업이 완료되면 사용
+    userEmail_receive = request.form['userEmail_give']
+    userName_receive = request.form['userName_give']
+    user_sign_ID_receive = request.form['user_sign_ID_give']
+    user_sign_PW_receive = request.form['user_sign_PW_give']
+
+    # PW 해쉬 적용
+    pw_hash = hashlib.sha256(user_sign_PW_receive.encode('utf-8')).hexdigest()
+
+    db.user.insert_one(
+        {'email': userEmail_receive, 'name': userName_receive, 'id': user_sign_ID_receive, 'pw': pw_hash})
+
+    return jsonify({'result': 'success'})
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')  # 로그인 화면으로 이동
+
+
+@app.route('/sign_up')
+def login_after_sign_up():
+    return render_template('sign_up.html')  # 회원가입 로그인 화면으로 이동
 
 
 @app.route('/mypage')
