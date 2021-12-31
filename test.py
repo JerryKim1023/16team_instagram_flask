351e8760fdf5acce645cbdb5a6bf78086d4b843d
heyhyo11 <1326cy@gmail.com>
2021-12-31 10:30:20 +0900

    충돌수정

--- test.py
+++ test.py
@@ -1,29 +1,39 @@
+from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
 import certifi
 ca = certifi.where()
-client = MongoClient('mongodb+srv://AKBARI:sparta@cluster0.jujbu.mongodb.net/cluster0?retryWrites=true&w=majority', tlsCAFile = ca)
+app = Flask(__name__)
+client = MongoClient('mongodb+srv://AKBARI:sparta@cluster0.jujbu.mongodb.net/cluster0?retryWrites=true&w=majority', tlsCAFile=ca)
 db = client.dbakbari
 
-from flask import Flask, render_template, jsonify
-app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('test.html')
+    return render_template('test2.html')
 
-# 남효정: 테스트용, 나중에 삭제예정
-@app.route('/test', method=['POST'])
-def test_post():
-    img_receive= request.form['img_give']
-    comment_receive = request.form['comment_give']
-
-    doc = {
-        'img': img_receive,
-        'comment': comment_receive
+@app.route("/bucket", methods=["POST"])
+def bucket_post():
+    comment_receive = request.form['comment_give'] # [POST] - 1
+    # comment_list = list(db.comment.find({}, {'_id': False})) # [POST] - 3
+    # count = len(comment_list) + 1
+    doc = { # [POST] - 2
+        # 'user_id': user_id_receive,
+        # 'num': count, # 댓글 순번
+        'comment': comment_receive,
     }
+    db.comment.insert_one(doc)
+    return jsonify({'msg': '등록 완료!'})
 
-    doc.rest.insert_one(doc)
 
-    return jsonify({'msg': 'POST 업로드 완료!'})
-    # return render_template('test.html') # 테스트 작업이 완료되면 사용
-    # return 'This is a test'
+@app.route("/bucket", methods=["GET"])
+def bucket_get():
+    comment_list = list(db.comment.find({}, {'_id': False})) # [GET] - 1
+    return jsonify({'comments': comment_list}) # [GET] - 2
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
