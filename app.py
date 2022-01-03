df0c8ad00b7e244d3f8ccd70bd09791e6496749f
JerryKim <ecec1023@gmail.com>
2022-01-03 11:41:02 +0900

    수정 - app.py 로컬 5000 다시열어서 풀음

--- app.py
+++ app.py
@@ -182,5 +182,5 @@ def show_mainpost():
 
 
 # 서버 올릴 때는 없애고 올려주기!!!
-# if __name__ == '__main__':
-#     app.run('0.0.0.0', port=5000, debug=True)
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
